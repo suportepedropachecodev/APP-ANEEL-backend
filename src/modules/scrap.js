24c4d3148159b3aa72d95cdc727f8f662f4a8784
Pedro Alexandre Pacheco <pedro.pacheco.a@gmail.com>
2020-09-02 15:46:58 -0300

    Criando scrap

---
+++ src/modules/scrap.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let dados = {};
+
+async function scrap(url){
+    const {data} = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    for(i=6; i<1006;i++){
+        let distibuidora = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(1)`).text().trim();
+        let codigo = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(2)`).text().trim();
+        let titular = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
+        let classe = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(4)`).text().trim();
+        let subgrupo = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(5)`).text().trim();
+        let modalidade = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(6)`).text().trim();
+        let credito = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(7)`).text().trim();
+        let municipio = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(8)`).text().trim();
+        let uf = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(9)`).text().trim();
+        let cep = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(10)`).text().trim();
+        let data = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(11)`).text().trim();
+        let tipo = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(12)`).text().trim();
+        let fonte = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(13)`).text().trim();
+        let potencia = $(`body > table:nth-child(4) > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:nth-child(${i}) > td:nth-child(14)`).text().trim();
+    dados = {
+        distibuidora,
+        codigo,
+        titular,
+        classe,
+        subgrupo,
+        modalidade,
+        credito,
+        municipio,
+        uf,
+        cep,
+        data,
+        tipo,
+        fonte,
+        potencia
+    }
+    console.log(dados);
+    }
+};
+
+scrap('http://www2.aneel.gov.br/scg/gd/VerGD.asp?pagina=2&acao=buscar&login=&NomPessoa=&IdAgente=&DatConexaoInicio=&DatConexaoFim=')
